5c30efc420c5946e1c8d1582c8233f7b0b3fcd7d
godisdead1729 <80743602+johnnynguyen98@users.noreply.github.com>
2026-06-01 19:00:39 +1000

    Update main.py

--- main.py
+++ main.py
@@ -1,38 +1,41 @@
-# To run and test the code you need to update 4 places:
-# 1. Change MY_EMAIL/MY_PASSWORD to your own details.
-# 2. Go to your email provider and make it allow less secure apps.
-# 3. Update the SMTP ADDRESS to match your email provider.
-# 4. Update birthdays.csv to contain today's month and day.
-# See the solution video in the 100 Days of Python Course for explainations.
-
-
 from datetime import datetime
 import pandas
 import random
 import smtplib
 import os
 
-# import os and use it to get the Github repository secrets
 MY_EMAIL = os.environ.get("MY_EMAIL")
 MY_PASSWORD = os.environ.get("MY_PASSWORD")
 
-today = datetime.now()
-today_tuple = (today.month, today.day)
+birthday_people = {}
 
-data = pandas.read_csv("birthdays.csv")
-birthdays_dict = {(data_row["month"], data_row["day"])                  : data_row for (index, data_row) in data.iterrows()}
-if today_tuple in birthdays_dict:
-    birthday_person = birthdays_dict[today_tuple]
-    file_path = f"letter_templates/letter_{random.randint(1, 3)}.txt"
-    with open(file_path) as letter_file:
-        contents = letter_file.read()
-        contents = contents.replace("[NAME]", birthday_person["name"])
+def birthday_checker(datafile):
+    global birthday_names
+    birthday_file = pd.read_csv(datafile)
+    current_date = (dt.datetime.now().month, dt.datetime.now().day)
+    for index, row in birthday_file.iterrows():
+        if current_date == (int(row['month']),int(row['day'])):
+            birthday_people[row['name']] = row['email']
+    if len(birthday_people)>0:
+        return True
 
-    with smtplib.SMTP("YOUR EMAIL PROVIDER SMTP SERVER ADDRESS") as connection:
+def create_letter(name):
+    with open(f"letter_templates/letter_{random.randint(1,3)}.txt","r") as letter:
+        letter_template = letter.read()
+        letter_content = letter_template.replace("[NAME]",name)
+        letter_content = letter_content.replace("Angela","Johnny")
+    return letter_content
+
+def send_email(name):
+    with smtplib.SMTP("smtp.gmail.com", 587) as connection:
         connection.starttls()
-        connection.login(MY_EMAIL, MY_PASSWORD)
-        connection.sendmail(
-            from_addr=MY_EMAIL,
-            to_addrs=birthday_person["email"],
-            msg=f"Subject:Happy Birthday!\n\n{contents}"
-        )
+        connection.login(user=MY_EMAIL, password=PASSWORD)
+        connection.sendmail(from_addr=MY_EMAIL,
+                            to_addrs=birthday_people[name],
+                            msg=f"Subject:Happy Birthday!\n\n{create_letter(name)}"
+                            )
+        
+if birthday_checker("birthdays_updated.csv"):
+    for name in birthday_people:
+        send_email(name)
+
